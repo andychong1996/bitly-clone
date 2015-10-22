3e2597d23925e1fae5faa31489df0504f2dd5613
andychong1996 <andygg1996personal@gmail.com>
2015-10-22 18:09:37 +0800

    added click_count column, set it to 0, added css, added layout.erb, modified index.erb, added increment method for click_count column in  url model, added favicon

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,19 +1,28 @@
 require_relative '../models/url.rb'
 
 get '/' do
-	@urls = Url.all
+	@urls = Url.all.order(:id)
  	erb :"static/index"
 end
 
 post '/urls' do
-	
-	Url.new(long_url: params[:url][:long]).save
-	redirect to '/'
-	# erb :"static/index"
+
+	@current_url = Url.new(long_url: params[:url][:long])
+
+	if @current_url.save
+		redirect to '/'
+	elsif @current_url.errors.messages
+		@urls = Url.all
+		erb :"static/index"
+	else
+		@current_url.save
+		redirect to '/'
+	end
 end
 
 get '/:short_url'  do
 	#redirect user to the original long_url
 	url = Url.find_by(short_url: params[:short_url])
-	redirect to "#{url.long_url}" 
+	url.add_count
+	redirect to "#{url.long_url}"
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,13 +1,16 @@
 class Url < ActiveRecord::Base
 	before_create :shorten
 
-	# def initialize(data={})
-	# 	@long_url = data.fetch(long_url)
-	# 	@short_url
-	# end
+	validates :long_url, presence: true
+	validates :long_url, format: { with: URI.regexp }
+	validates :short_url, uniqueness: true
 
 	def shorten
 		self.short_url = (('a'..'z').to_a.shuffle[0,6]+(0..9).to_a.shuffle[0,2]).shuffle.join
+	end
+
+	def add_count
+			self.increment!(:click_count, 1)
 	end
 
 end
