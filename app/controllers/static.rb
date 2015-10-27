561d6c451bee48ad0ffc2f7b0c03896f823f5af6
andychong1996 <andygg1996personal@gmail.com>
2015-10-27 11:33:28 +0800

    add lots of new stuff! but the css still sucks

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,22 +1,20 @@
 require_relative '../models/url.rb'
 
 get '/' do
-	@urls = Url.all.order(:id)
+	@urls = (Url.all.order(id: :desc)).limit(20)
  	erb :"static/index"
 end
 
 post '/urls' do
 
-	@current_url = Url.new(long_url: params[:url][:long])
-
+	@current_url = Url.new( params[:url])
 	if @current_url.save
-		redirect to '/'
-	elsif @current_url.errors.messages
-		@urls = Url.all
-		erb :"static/index"
+		@url = (Url.all.order(id: :desc)).limit(20).to_json
 	else
-		@current_url.save
-		redirect to '/'
+		@status = false
+		@status.to_json
+		# @urls = Url.all.to_json
+		# erb :"static/index"
 	end
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,49 @@
 $(document).ready(function(){
 
-  
+  $("#reminder").hide();
+  $("#urls_table").hide();
 
+  $("#submit_button").mouseenter(function(){
+    $(this).fadeTo('fast', 1);
+    $("#reminder").fadeIn();
+  });
 
+  $("#submit_button").mouseleave(function(){
+      $(this).fadeTo('fast', 0.9);
+      $("#reminder").fadeOut();
+  });
+
+  $("#table_title_button").click(function(){
+    $("#urls_table").slideToggle();
+  });
+
+  $("#url_input_form").submit(function(event){
+    event.preventDefault();
+    $(this).validate();
+// send ajax to server
+    $.ajax({
+      type: $(this).attr("method"),
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+    }).done(function(return_data){
+      // if done, run if else
+
+      if(return_data != "false"){
+         var  data = JSON.parse(return_data);
+         $('table#urls_table > tbody').html("")
+        $.each(data, function(id, whole_data){
+
+          $('table#urls_table > tbody').append("<tr>\
+                                        <td><a href=\" "+ whole_data.long_url + "\">" + whole_data.long_url + "</a></td>\
+                                        <td><a href= \"http://localhost:9393/" + whole_data.short_url + " \">localhost:9393/" + whole_data.short_url + "</a></td>\
+                                        <td id=\"click_count_col\">" + whole_data.click_count + "</td>\
+                                      </tr>");
+        });
+      }else{
+        $("div#error_message").html("");
+        $('div#error_message').append("Please insert a valid URL!");
+      };
+
+    });
+  });
 });
