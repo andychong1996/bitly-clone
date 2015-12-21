2493291aae5429996b4923df0fc90972b73bbb03
andychong1996 <andygg1996personal@gmail.com>
2015-12-21 12:59:32 +0800

    added joined public folder to root

--- config/environments/init.rb
+++ config/environments/init.rb
@@ -43,6 +43,8 @@ require APP_ROOT.join('config', 'database')
 # Setup views (V)
 set :views, File.join(APP_ROOT, "app", "views")
 
+#Setup public folder
+set :public_folder, File.join(APP_ROOT, "public")
 # Setup helper (H)
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
